35e335468307a8bd5973081eda2773f5e9bfb003
Nemirtingas <nanaki89@hotmail.fr>
2020-05-15 22:55:03 +0200

    Match cmake convention

--- cmake/FindZSTD.cmake
+++ cmake/FindZSTD.cmake
@@ -1,12 +1,10 @@
 
-if(USE_ZSTD_COMPRESS)
-  find_library(
-    ZSTD_LIBRARIES
-    NAMES zstd_static zstd
-  )
-  
-  find_path(
-    ZSTD_INCLUDE
-    zstd.h
-  )
-endif()
+find_library(
+  ZSTD_LIBRARIES
+  NAMES zstd_static zstd
+)
+
+find_path(
+  ZSTD_INCLUDE_DIRS
+  zstd.h
+)
