62fa2a54e06f63151519e45fd1e4e221c5161264
Nemirtingas <nanaki89@hotmail.fr>
2020-06-29 21:39:41 +0200

    Fix copy bug

--- eos_dll/eossdk_friends.cpp
+++ eos_dll/eossdk_friends.cpp
@@ -62,7 +62,7 @@ void EOSSDK_Friends::QueryFriends(const EOS_Friends_QueryFriendsOptions* Options
 
     _friends.clear();
 
-    auto users = GetEOS_Connect()._users;
+    auto& users = GetEOS_Connect()._users;
     for (auto user_it = ++users.begin(); user_it != users.end(); ++user_it)
     {
         if (user_it->second.authentified)
