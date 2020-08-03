d6ea55befe6e3b20962cbbe23873a4e813cb8f01
Nemirtingas <nanaki89@hotmail.fr>
2020-08-03 22:18:44 +0200

    Working on a launcher

---
+++ NemirtingasEmuLauncher/OSUtility/OsId.cs
@@ -0,0 +1,14 @@
+﻿namespace OSUtility
+{
+    public enum OsId
+    {
+        Unknown,
+        Windows,
+        Linux,
+        MacOSX,
+        Unix,
+        //AIX,
+        //HP_UX,
+        //FreeBSD,
+    }
+}
